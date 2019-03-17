3e6ca2ca137d3070ce5c8170340f86794af93094
Harsh Vardhan <harshv47@gmail.com>
2019-03-17 12:15:32 +0530

    Delete deploy_histories.js

--- migrations/deploy_histories.js
+++
@@ -1,6 +0,0 @@
-const config = require('../truffle'),
-    Organizations = artifacts.require('./Organizations.sol'),
-    Histories = artifacts.require('./Histories.sol');
-
-module.exports = function(deployer, network) {
-    deployer.deploy(Histories, config.networks[network].gmoCns, Organizations.address)};
